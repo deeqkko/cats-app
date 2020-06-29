a6f8e4a4ee6045f87a4018f8c6f12436a291bdd6
deeqkko <kimmo.koivusalo@gmail.com>
2020-06-29 11:57:55 +0300

    testing

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4c1wxw($svnii&h)xjq0^pmqp7dohx(gx!dnpc7_4i^3%$4^%@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
